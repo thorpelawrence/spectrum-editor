0c7d6e4d478bdcb841f81bf6feb07ba238b64108
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2017-07-23 23:43:20 +0100

    Cleanup log

--- main.js
+++ main.js
@@ -36,13 +36,6 @@ log.info("App starting...")
 autoUpdater.logger = log
 autoUpdater.logger.transports.file.level = 'info'
 
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message);
-})
-
 autoUpdater.on('update-downloaded', (info) => {
     autoUpdater.quitAndInstall()
 })
